50717a242913b02bdb3c2b8967a46d6e50c83607
plee99 <peter_lee_16@sfu.ca>
2023-06-12 15:56:26 -0700

    push

---
+++ src/test/java/com/cmpt276/a2/A2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmpt276.a2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class A2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
